ad598337c1bc96c4f160c4d1aad9826c60da50cb
Yassine Fathi <Yassine_Fathi@outlook.com>
2019-04-10 12:51:40 +0100

    Changed the encoding algorithm

--- src/main/java/dev/m4tt72/jwtoken/utils/Util.java
+++ src/main/java/dev/m4tt72/jwtoken/utils/Util.java
@@ -9,34 +9,28 @@ import javax.crypto.spec.SecretKeySpec;
 import dev.m4tt72.jwtoken.models.Header;
 
 public class Util {
-	public static String base64Encode(String str) throws UnsupportedEncodingException {
-		byte[] bytes = str.getBytes("UTF-8");
+	
+	public static String base64Encode(String str) {
+		return base64Encode(str.getBytes());
+	}
+
+	public static String base64Encode(byte[] bytes) {
 		return Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
 	}
 	
-	public static String base64Decode(String str) {
-		byte[] decoded = Base64.getUrlDecoder().decode(str);
+	public static String base64Decode(String base64) {
+		byte[] decoded = Base64.getUrlDecoder().decode(base64);
 		return new String(decoded);
 	}
 	
-	public static String createHmac(Header header, String msg, String sec) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
-		byte[] key = sec.getBytes("UTF-8");
+	public static String createHmac(Header header, String msg, String sec) throws NoSuchAlgorithmException, InvalidKeyException {
+		byte[] key = sec.getBytes();
 		String algorithm = "HmacSHA" + header.getAlg().substring(2);
 		Mac hmac = Mac.getInstance(algorithm);
 		SecretKeySpec keySpec = new SecretKeySpec(key, algorithm);
 		hmac.init(keySpec);
-		String result = bytesToHex(hmac.doFinal(msg.getBytes("UTF-8")));
-		return base64Encode(result);
+		String result = base64Encode(hmac.doFinal(msg.getBytes()));
+		return result;
 	}
 	
-	public static String bytesToHex(byte[] bytes) {
-        final  char[] hexArray = "0123456789ABCDEF".toCharArray();
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
 }
